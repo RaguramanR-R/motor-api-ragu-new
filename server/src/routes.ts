7d77620569a98e835b7f7f83e355cb01143d4625
Raguraman . <ragur@sutherlandglobal.com>
2026-09-16 10:58:34 +0530

    333111

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,17 +1,9 @@
 //CORE_REFERENCE_IMPORTS
 //CORE_REFERENCE_IMPORT-motor_poc-SSD_SERVICE_ID_sd_mlJ0xjSZMe9QOv8p
 import { motor_poc as SSD_SERVICE_ID_sd_mlJ0xjSZMe9QOv8p } from './sd-services/motor_poc';
-//CORE_REFERENCE_IMPORT-idsutil-sd_FYsOjosGFbsl8g7J
-import { idsutil as sd_FYsOjosGFbsl8g7J } from './sd-services/idsutil';
-//CORE_REFERENCE_IMPORT-ids-sd_sNCkY9BHjsjtSYvx
-import { ids as sd_sNCkY9BHjsjtSYvx } from './sd-services/ids';
 
 export const UserRoutes = [
 //CORE_REFERENCE_PUSH_TO_ROUTE_ARRAY
 //CORE_REFERENCE_PUSH_TO_ROUTE_ARRAY-motor_poc-SSD_SERVICE_ID_sd_mlJ0xjSZMe9QOv8p
 SSD_SERVICE_ID_sd_mlJ0xjSZMe9QOv8p,
-//CORE_REFERENCE_PUSH_TO_ROUTE_ARRAY-idsutil-sd_FYsOjosGFbsl8g7J
-sd_FYsOjosGFbsl8g7J,
-//CORE_REFERENCE_PUSH_TO_ROUTE_ARRAY-ids-sd_sNCkY9BHjsjtSYvx
-sd_sNCkY9BHjsjtSYvx,
 ];
